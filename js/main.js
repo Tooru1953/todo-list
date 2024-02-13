835a67a5f61109b4bb073d6d9847bc31ac062af4
Stanislav <datura1953@mail.ru>
2024-02-13 20:28:15 +0700

    final

--- js/main.js
+++ js/main.js
@@ -6,6 +6,32 @@ const emptyList = document.querySelector("#emptyList");
 
 let tasks = [];
 
+if (localStorage.getItem("tasks")) {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+tasks.forEach(function (task) {
+  const cssClass = task.done ? "task-title task-title--done" : "task-title";
+
+  // формируем разметку для новой задачи
+  const taskHTML = `<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+  <span class="${cssClass}">${task.text}</span>
+  <div class="task-item__buttons">
+    <button type="button" data-action="done" class="btn-action">
+      <img src="./img/tick.svg" alt="Done" width="18" height="18" />
+    </button>
+    <button type="button" data-action="delete" class="btn-action">
+      <img src="./img/cross.svg" alt="Done" width="18" height="18" />
+    </button>
+  </div>
+  </li>`;
+
+  // добавляем задачу на страницу
+  tasksList.insertAdjacentHTML("beforeend", taskHTML);
+});
+
+checkEmptyList();
+
 // добавление задачи
 form.addEventListener("submit", addTask);
 
@@ -31,6 +57,8 @@ function addTask(event) {
   // добавляем задачу в массив с задачами
   tasks.push(newTask);
 
+  saveToLocalStorage();
+
   // формируем css класс
   const cssClass = newTask.done ? "task-title task-title--done" : "task-title";
 
@@ -54,9 +82,7 @@ function addTask(event) {
   taskInput.value = "";
   taskInput.focus();
 
-  if (tasksList.children.length > 1) {
-    emptyList.classList.add("none");
-  }
+  checkEmptyList();
 }
 
 function deleteTask(event) {
@@ -76,13 +102,13 @@ function deleteTask(event) {
     // удаляем задачу из массива с задачами
     tasks.splice(index, 1);
 
+    saveToLocalStorage();
+
     // удаляем задачу из разметки
     parentNode.remove();
-
-    if (tasksList.children.length === 1) {
-      emptyList.classList.remove("none");
-    }
   }
+
+  checkEmptyList();
 }
 
 function doneTask(event) {
@@ -101,7 +127,28 @@ function doneTask(event) {
 
     task.done = !task.done;
 
+    saveToLocalStorage();
+
     const taskTitle = parentNode.querySelector(".task-title");
     taskTitle.classList.toggle("task-title--done");
   }
+}
+
+function checkEmptyList() {
+  if (tasks.length === 0) {
+    const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+    <img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3" />
+    <div class="empty-list__title">Список дел пуст</div>
+  </li>`;
+    tasksList.insertAdjacentHTML("afterbegin", emptyListHTML);
+  }
+
+  if (tasks.length > 0) {
+    const emptyListEl = document.querySelector("#emptyList");
+    emptyListEl ? emptyListEl.remove() : null;
+  }
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
